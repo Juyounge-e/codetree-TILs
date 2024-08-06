d8a45f38acd59b44066e1efb6a2773ea7506eaac
Juyounge-e <130207280+Juyounge-e@users.noreply.github.com>
2024-08-06 22:12:10 +0900

    Title: 그 요일은, Time: 133ms, Memory: 24MB, Status: Passed - CodeTree

--- 240806/그 요일은/the-day-of-the-day.py
+++ 240806/그 요일은/the-day-of-the-day.py
@@ -1,33 +1,15 @@
-# 매 월 마지막일
-leap_days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-
-# 요일 리스트 
-weekdays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-
-# 각 날짜가 해당 연도의 몇번째 날인지 구하는 함수
-def cal_days(m, d):
-    cnt = 0
-    for i in range(1, m-1):
-        cnt += leap_days[i]
-    cnt += d
-    return cnt
-
-def howmanyweekday(m1, d1, m2, d2, day):
-    start_cnt = cal_days(m1, d1)
-    end_cnt = cal_days(m2, d2)
-
-    # 날짜 차이 계산
-    diff = end_cnt - start_cnt
-    
-    cnt = 0
-    number = diff - weekdays.index(day)
-
-    # 차이일을 7로 나눈 몫을 계산하여 특정 요일의 발생 횟수 계산
-    cnt = (number + 7) // 7
-    return cnt
-
-# 입력
 m1, d1, m2, d2 = map(int, input().split())
-day = str(input())
 
-print(howmanyweekday(m1, d1, m2, d2, day))
+A = input()
+
+#                  1.  2.  3.  4.  5.  6.  7.  8.  9. 10. 11. 12.
+num_of_days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+day = ["Mon", 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+diff = sum(num_of_days[1:m2]) + d2 - (sum(num_of_days[1:m1]) + d1)
+
+cnt = 0
+number = diff - day.index(A)
+
+cnt = (number + 7) // 7
+
+print(cnt)
