d8654f644c9f2154202489b90f3e72f4e0e87f27
Juyounge-e <130207280+Juyounge-e@users.noreply.github.com>
2024-08-06 22:44:56 +0900

    Title: 요일 맞추기, Time: 129ms, Memory: 24MB, Status: Passed - CodeTree

--- 240806/요일 맞추기/guess-day-of-week.py
+++ 240806/요일 맞추기/guess-day-of-week.py
@@ -5,11 +5,12 @@ weekday = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 
 # m2,d2은 무슨 요일인지 구하기 
 def whatday(m1, d1, m2, d2):
-    diff = sum(days[1:m2]) + d2 - sum(days[1:m1]) + d1
-    diff %= 7
+    diff = (sum(days[1:m2]) + d2) - (sum(days[1:m1]) + d1)
     
-    day = weekday[diff-1]
-    return day
+    day_index = (diff%7 + 7)%7
+
+    return weekday[day_index]    
+    
 
 m1, d1, m2, d2 = map(int, input().split())
 print(whatday(m1, d1, m2, d2))
